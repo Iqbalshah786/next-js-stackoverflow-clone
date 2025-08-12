74404f01c0efcb679292c917aa5ff4edf49cdc87
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-08-13 02:08:42 +0500

    Add SignIn

--- lib/handlers/actions/auth.action.ts
+++ lib/handlers/actions/auth.action.ts
@@ -2,13 +2,14 @@
 
 import { ActionResponse, ErrorResponse } from "@/types/global";
 import action from "../action";
-import { SignUpSchema } from "@/lib/validations";
 import handleError from "../error";
 import User from "@/Database/user.modes";
 import bcrypt from "bcryptjs";
 import Account from "@/Database/account.models";
 import { signIn } from "@/auth";
 import mongoose from "mongoose";
+import { SignInSchema, SignUpSchema } from "@/lib/validations";
+import { NotFoundError } from "@/lib/http-errors";
 
 export async function signUpWithCredentials(
   params: AuthCredentials
@@ -61,5 +62,41 @@ export async function signUpWithCredentials(
     return handleError(error) as ErrorResponse;
   } finally {
     await session.endSession();
+  }
+}
+
+export async function signInWithCredentials(
+  params: Pick<AuthCredentials, "email" | "password">
+): Promise<ActionResponse> {
+  const validationResult = await action({ params, schema: SignInSchema });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+  const { email, password } = validationResult.params!;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    console.log(existingUser);
+    if (!existingUser) throw new NotFoundError("User");
+
+    const existingAccount = await Account.findOne({
+      provider: "credentials",
+      providerAccountId: email,
+    });
+
+    if (!existingAccount) throw new NotFoundError("Account");
+
+    const passwordMached = await bcrypt.compare(
+      password,
+      existingAccount.password!
+    );
+    if (!passwordMached) throw new Error("Invalid credentials");
+
+    await signIn("credentials", { email, password, redirect: false });
+    return { success: true };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
   }
 }
